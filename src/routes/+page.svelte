cef1c2163ebc4c76e85941fc842152913aad4737
Mark Burggraf <markb@mantisbible.com>
2023-11-15 06:09:01 -0800

    working basic flow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,9 @@
 	import data from './data.json';
 	let q: string = '';
 	let a: string = '';
+	let score: number = 0;
+	let total: number = 0;
+	let result: string = '';
 	const getQuestion = () => {
 		const index = Math.floor(Math.random() * data.length);
 		const q = data[index];
@@ -13,6 +16,24 @@
 		const arr = qq.split('|');
 		q = arr[0];
 		a = arr[1];
+	}
+	const choose = (choice: string) => {
+		if (a === choice) {
+			finish(true);
+		} else {
+			finish(false);
+		}
+		loadQuestion();
+	}
+	const finish = (scored: boolean) => {
+		if (scored) {
+			score++;
+			result = `Correct! <b>${q}</b> is a <b>${q=="D" ? "drug" : "planet"}</b>.`;
+		} else {
+			result = `Incorrect! <b>${q}</b> is a <b>${q=="D" ? "drug" : "planet"}</b>.`;
+		}
+		total++;
+		loadQuestion();
 	}
 	loadQuestion();
 </script>
@@ -26,11 +47,21 @@
 		<ion-grid>
 			<ion-row>
 				<ion-col>
-					<ion-button color="danger" expand="block" on:click={loadQuestion}>Drug</ion-button>
+					{@html result}
+				</ion-col>
+			</ion-row>
+			<ion-row>
+				<ion-col>
+					{score} / {total}
+				</ion-col>
+			</ion-row>
+			<ion-row>
+				<ion-col>
+					<ion-button color="danger" expand="block" on:click={()=>{choose('D')}}>Drug</ion-button>
 				</ion-col>
 				<ion-col>
-					<ion-button color="success" expand="block" on:click={loadQuestion}>Planet</ion-button>
-				</ion-col>
+					<ion-button color="success" expand="block" on:click={()=>{choose('P')}}>Planet</ion-button>
+				</ion-col>				
 			</ion-row>
 		</ion-grid>
 	</ion-card-content>
